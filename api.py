3122919506d0d09be41446b2cf3e3f3b31d89c19
Евгений Титов <ztcccp4568@mail.ru>
2022-03-07 14:41:22 +0300

    Third lab commit. Update route check and handle_request for check class

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-# api.py
+import inspect
 
 from parse import parse
 from webob import Request, Response
@@ -10,12 +10,12 @@ class API:
 
     def __call__(self, environ, start_response):
         request = Request(environ)
-
         response = self.handle_request(request)
 
         return response(environ, start_response)
 
     def route(self, path):
+        assert path not in self.routes, "Such route already exists."
         def wrapper(handler):
             self.routes[path] = handler
             return handler
@@ -36,10 +36,14 @@ class API:
 
     def handle_request(self, request):
         response = Response()
-
         handler, kwargs = self.find_handler(request_path=request.path)
 
         if handler is not None:
+            if inspect.isclass(handler):
+                handler = getattr(handler(), request.method.lower(), None)
+                if handler is None:
+                    raise AttributeError("Method not allowed", request.method)
+
             handler(request, response, **kwargs)
         else:
             self.default_response(response)
