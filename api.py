a31d59a0485db21c7ba2a0f3f12da53127b7c78b
Евгений Титов <ztcccp4568@mail.ru>
2022-03-22 22:52:28 +0300

    Eight lab commit. Update handlers

--- api.py
+++ api.py
@@ -41,40 +41,50 @@ class API:
 
         return response(environ, start_response)
 
-    def add_route(self, path, handler):
+    def add_route(self, path, handler, allowed_methods=None):
         assert path not in self.routes, "Such route already exists."
 
-        self.routes[path] = handler
+        if allowed_methods is None:
+            allowed_methods = ['get', 'post', 'put', 'patch', 'delete', 'options']
 
-    def route(self, path):
+        self.routes[path] = {"handler": handler, "allowed_methods": allowed_methods}
+
+
+    def route(self, path, allowed_methods=None):
         def wrapper(handler):
-            self.add_route(path, handler)
+            self.add_route(path, handler, allowed_methods)
             return handler
 
         return wrapper
+
 
     def default_response(self, response):
         response.status_code = 404
         response.text = "Not found."
 
     def find_handler(self, request_path):
-        for path, handler in self.routes.items():
+        for path, handler_data in self.routes.items():
             parse_result = parse(path, request_path)
             if parse_result is not None:
-                return handler, parse_result.named
+                return handler_data, parse_result.named
 
         return None, None
 
     def handle_request(self, request):
         response = Response()
 
-        handler, kwargs = self.find_handler(request_path=request.path)
+        handler_data, kwargs = self.find_handler(request_path=request.path)
 
         try:
-            if handler is not None:
+            if handler_data is not None:
+                handler = handler_data["handler"]
+                allowed_methods = handler_data["allowed_methods"]
                 if inspect.isclass(handler):
                     handler = getattr(handler(), request.method.lower(), None)
                     if handler is None:
+                        raise AttributeError("Method not allowed", request.method)
+                else:
+                    if request.method.lower() not in allowed_methods:
                         raise AttributeError("Method not allowed", request.method)
 
                 handler(request, response, **kwargs)
